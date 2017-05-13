c4a9f6064ed63b29058c723b6cc6e0097c4df59f
Paul Seyfert <paul.seyfert@mib.infn.it>
2017-05-13 17:53:19 +0200

    official test

--- Makefile
+++ Makefile
@@ -1,24 +1,15 @@
-
-threadtest.o: threadtest.cc
-	g++  -Irapidcheck/include -march=native -std=c++14 -m64 -O2 -g -Wextra -Wall  -c -o $@ $^
+all: test
+	./test
 
 test.o: test.cc
 	g++  -Irapidcheck/include -march=native -std=c++14 -m64 -O2 -g -Wextra -Wall  -c -o $@ $^
 
-threadtest: threadtest.o
+test: test.o rapidcheck
 	g++ -pthread -lstdc++ -m64 -g -march=native -flto  $^ rapid.build/librapidcheck.a -o $@
-
-test: test.o
-	g++ -pthread -lstdc++ -m64 -g -march=native -flto  $^ rapid.build/librapidcheck.a -o $@
-
-%.o: %.cc
-	g++  -march=native -std=c++11 -m64 -O2 -g -Wextra -Wall -Wshadow  -c -o $@ $^
-
-%: %.o
-	g++ -pthread -lstdc++ -m64 -g -march=native -flto  $^   -o $@
 
 rapidcheck:
 	mkdir -p rapid.build
-	(cd rapid.build; cmake ../rapidcheck -DCMAKE_INSTALL_PREFIX=../install; cmake --build .; cmake --target install)
+	mkdir -p install
+	(cd rapid.build && cmake ../rapidcheck -DCMAKE_INSTALL_PREFIX=../install && cmake --build .)
 
 .PHONY: rapidcheck
--- test.cc
+++ test.cc
@@ -6,43 +6,46 @@
 
 #include "rapidcheck.h"
 
-namespace safe
-{
-#include "./safe/TMVA_MLP_Forward1stLoop.h"
+#ifndef IClassifierReader__def
+#define IClassifierReader__def
+
+class IClassifierReader {
+
+public:
+  // constructor
+  IClassifierReader() : fStatusIsClean(true) {}
+  virtual ~IClassifierReader() {}
+
+  // return classifier response
+  virtual double GetMvaValue(const std::vector<double> &inputValues) const = 0;
+
+  // returns classifier status
+  bool IsStatusClean() const { return fStatusIsClean; }
+
+protected:
+  bool fStatusIsClean;
+};
+
+#endif
+namespace safe {
+#include "thenew.class.C"
 }
-namespace reference
-{
-#include "./reference/TMVA_MLP_Forward1stLoop.h"
+namespace reference {
+#include "theref.class.C"
 }
 
-int main()
-{
-  rc::check( "GetMvaValue__", []( const std::array<float, 7>& l0 ) {
-    safe::ReadMLP_Forward1stLoop* saf =
-        new safe::ReadMLP_Forward1stLoop( {"nPlanes", "dSlope", "dp", "slope2", "dby", "dbx", "day"} );
-    reference::ReadMLP_Forward1stLoop* ref =
-        new reference::ReadMLP_Forward1stLoop( {"nPlanes", "dSlope", "dp", "slope2", "dby", "dbx", "day"} );
-    std::vector<float> input;
-    input.insert( input.begin(), l0.begin(), l0.end() );
-    float refval = ref->GetMvaValue__( input );
-    float safval = saf->GetMvaValue__( input );
+int main() {
+  rc::check("GetMvaValue", [](const std::array<double, 4> &l0) {
+    std::vector<std::string> vars({"var1+var2", "var1-var2", "var3", "var4"});
+    safe::ReadMLP *saf = new safe::ReadMLP(vars);
+    reference::ReadMLP *ref = new reference::ReadMLP(vars);
+    std::vector<double> input;
+    input.insert(input.begin(), l0.begin(), l0.end());
+    float refval = ref->GetMvaValue(input);
+    float safval = saf->GetMvaValue(input);
 
-    RC_ASSERT( refval == safval );
-  } );
-  rc::check( "GetMvaValue", []( const std::array<float, 7>& l0 ) {
-    safe::ReadMLP_Forward1stLoop* saf =
-        new safe::ReadMLP_Forward1stLoop( {"nPlanes", "dSlope", "dp", "slope2", "dby", "dbx", "day"} );
-    reference::ReadMLP_Forward1stLoop* ref =
-        new reference::ReadMLP_Forward1stLoop( {"nPlanes", "dSlope", "dp", "slope2", "dby", "dbx", "day"} );
-    std::vector<float> input1;
-    std::vector<float> input2;
-    input1.insert( input1.begin(), l0.begin(), l0.end() );
-    input2.insert( input2.begin(), l0.begin(), l0.end() );
-    float refval = ref->GetMvaValue( input1 );
-    float safval = saf->GetMvaValue( input2 );
-
-    RC_ASSERT( refval == safval );
-  } );
+    RC_ASSERT(refval == safval);
+  });
 
   return 0;
 }
